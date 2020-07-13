1a7e3f327ac559df40f72aec71467fdcd7f779cd
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-13 15:32:23 -0700

    add code for determining legal attacks along a rank, file, or diagonal

--- Pieces/queen.cs
+++ Pieces/queen.cs
@@ -13,7 +13,43 @@ namespace Pieces.Queen
 
     public bool CanQueenAttack(int squareFile, int squareRank)
     {
-      
+      if (AttackAlongFile(squareFile))
+      {
+        return true;
+      }
+      else if (AttackAlongRank(squareRank))
+      {
+        return true;
+      }
+      else if (AttackAlongDiagonal(squareFile, squareRank))
+      {
+        return true;
+      }
+      return false;
+    }
+
+    private bool AttackAlongFile(int squareFile)
+    {
+      return (squareFile == _queenFile);
+    }
+
+    private bool AttackAlongRank(int squareRank)
+    {
+      return (squareRank == _queenRank);
+    }
+
+    private bool AttackAlongDiagonal(int squareFile, int squareRank)
+    {
+      int fileDifference = _queenFile - squareFile;
+      int rankDifference = _queenRank - squareRank;
+      if (fileDifference == rankDifference || fileDifference * -1 == rankDifference)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
