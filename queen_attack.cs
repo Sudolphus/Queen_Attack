10f83ea23c7630f9bedcfd6ebccdac67df9e7453
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-13 15:11:36 -0700

    add code for intake of queen coordinates
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- queen_attack.cs
+++ queen_attack.cs
@@ -6,7 +6,19 @@ namespace QueenMain
   {
     public static void Main()
     {
-      
+      int queenFile = 0;
+      int queenRank = 0;
+      do
+      {
+        Console.WriteLine("Input the File the Queen is on [1-8]");
+        string stringQueenFile = Console.ReadLine();
+        Console.WriteLine("Input the Rank the Queen is on [1-8]");
+        string stringQueenRank = Console.ReadLine();
+        queenFile = int.Parse(stringQueenFile);
+        queenRank = int.Parse(stringQueenRank);
+      }
+      while (queenFile < 1 || queenFile > 8 || queenRank < 1 || queenRank > 8);
+      Console.WriteLine(queenFile.ToString() + queenRank.ToString());
     }
   }
 }
